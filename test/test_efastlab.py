4a99b5619ef19710cf2fbfa7d27ff0a6630a7e02
michaeljpitcher <mjp22@st-andrews.ac.uk>
2019-08-28 12:25:53 +0100

    EFAST tests

--- test/test_efastlab.py
+++ test/test_efastlab.py
@@ -71,12 +71,40 @@ class EFASTLabTestCase(unittest.TestCase):
 
         # All we can check is value falls within expected range since random numbers are involved
         for row in ps:
-            for p, (min, max, dist) in params.iteritems():
+            for p, (a, b, dist) in params.iteritems():
                 if dist == UNIFORM_DISTRIBUTION:
-                    self.assertTrue(min <= row[p] <= max)
+                    self.assertTrue(a <= row[p] <= b)
                 if dist == NORMAL_DISTRIBUTION:
                     # TODO - testing a normal distribution
                     pass
+
+    def test_parameter_space_filtered(self):
+        # Test parameter space when we only want a few parameters
+        # Set params
+        params = {'a': (0, 10, UNIFORM_DISTRIBUTION),
+                  'b': (20, 0.5, NORMAL_DISTRIBUTION), 'c': (30, 0.6, LOGNORMAL_DISTRIBUTION),
+                  'd': (40, 0.7, LOGNORMAL_DISTRIBUTION), 'e': (50, 0.8, NORMAL_DISTRIBUTION),
+                  'f': (70, 80, UNIFORM_DISTRIBUTION)}
+
+        for k, v in params.iteritems():
+            self.lab[k] = v
+
+        # Fixed params
+        fixed_params = {'i': 99, 'j': 98}
+        for k, v in fixed_params.iteritems():
+            self.lab[k] = v
+
+        self.lab.set_sample_number(257)
+        self.lab.set_interference_factor(4)
+        self.lab.set_resample_number(5)
+
+        req_params = ['a','b','c']
+        self.lab.set_required_parameters(req_params)
+
+        ps = self.lab.parameterSpace()
+        self.assertEqual(len(ps), 257*5*3)
+        for sample in ps:
+            self.assertTrue(sample['parameter_of_interest'] in req_params)
 
 # TODO - testing cluster would require an ipcluster to be running
 
--- test/test_efastnotebook.py
+++ test/test_efastnotebook.py
@@ -58,20 +58,97 @@ class LotkaVolterraModel(epyc.Experiment):
         output = {LotkaVolterraModel.Q: pde_results[t_indexes[t]][0], LotkaVolterraModel.P: pde_results[t_indexes[t]][1]}
         return output
 
+
+class EFASTJSONNotebookTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.filename = 'efastlabtest.json'
+
+    # def tearDown(self):
+    #     # # Get rid of json file
+    #     if os.path.exists(self.filename):
+    #         os.remove(self.filename)
+
+    def test_analyse(self):
+        # nb_pre = EFASTJSONNotebook(self.filename, True)
+        # self.lab = EFASTLab(nb_pre)
+        #
+        # model = LotkaVolterraModel()
+        # self.lab[LotkaVolterraModel.PARAM_ALPHA] = [1.5, 0.01, NORMAL_DISTRIBUTION]
+        # self.lab[LotkaVolterraModel.PARAM_BETA] = [1, 0.2, NORMAL_DISTRIBUTION]
+        # self.lab[LotkaVolterraModel.PARAM_SIGMA] = [3, 0.2, NORMAL_DISTRIBUTION]
+        # self.lab[LotkaVolterraModel.PARAM_DELTA] = [1, 0.01, NORMAL_DISTRIBUTION]
+        #
+        # self.lab[LotkaVolterraModel.INIT_Q] = 10
+        # self.lab[LotkaVolterraModel.INIT_P] = 5
+        #
+        # model.set_time_params(20, 21)
+        #
+        # resamples = 5  # NR
+        # runs = 257  # NS
+        # mi = 4.0
+        #
+        # self.lab.set_sample_number(runs)
+        # self.lab.set_resample_number(resamples)
+        # self.lab.set_interference_factor(mi)
+        #
+        # print "Running Experiment"
+        # self.lab.runExperiment(RepeatedExperiment(model, 2))
+
+        nb_post = EFASTJSONNotebook(self.filename, False)
+        print "Analysing Experiment Results"
+        res_s1, res_st = nb_post.generate_sensitivity_indices()
+
+        s1_vals = [numpy.mean(res_s1[('prey',p)]) for p in ['alpha','beta','sigma','delta','dummy']]
+        st_vals = [numpy.mean(res_st[('prey',p)]) for p in ['alpha','beta','sigma','delta','dummy']]
+        N = len(s1_vals)
+        s1_Std = [2*numpy.std(res_s1[('prey',p)]) for p in ['alpha','beta','sigma','delta','dummy']]
+        st_Std = [2*numpy.std(res_s1[('prey',p)]) for p in ['alpha','beta','sigma','delta','dummy']]
+        ind = np.arange(N)  # the x locations for the groups
+        width = 0.8  # the width of the bars: can also be len(x) sequence
+
+        print s1_vals
+        print st_vals
+
+        p2 = plt.bar(ind, st_vals, width, bottom=[0, ] * N, yerr=st_Std)
+        p1 = plt.bar(ind, s1_vals, width, yerr=s1_Std)
+
+        plt.ylim((0,1))
+
+        plt.ylabel('eFAST sensitivity')
+        plt.title('Lotka-Volterra EFAST')
+        plt.xticks(ind, (r'$\alpha$', r'$\beta$', r'$\sigma$', r'$\delta$', r'$dummy$'))
+        plt.yticks(np.arange(0, 1, 0.1))
+        plt.legend((p1[0], p2[0]), (r'$S_1$', r'$S_T$'))
+
+        plt.savefig("EFAST_LV.png")
+
+        print 'alpha s1', stats.ttest_ind(res_s1[('prey','alpha')], res_s1[('prey','dummy')])
+        print 'beta s1',stats.ttest_ind(res_s1[('prey', 'beta')], res_s1[('prey', 'dummy')])
+        print 'sigma s1',stats.ttest_ind(res_s1[('prey', 'sigma')], res_s1[('prey', 'dummy')])
+        print 'delta s1',stats.ttest_ind(res_s1[('prey', 'delta')], res_s1[('prey', 'dummy')])
+
+        print 'alpha st',stats.ttest_ind(res_st[('prey', 'alpha')], res_st[('prey', 'dummy')])
+        print 'beta st',stats.ttest_ind(res_st[('prey', 'beta')], res_st[('prey', 'dummy')])
+        print 'sigma st',stats.ttest_ind(res_st[('prey', 'sigma')], res_st[('prey', 'dummy')])
+        print 'delta st',stats.ttest_ind(res_st[('prey', 'delta')], res_st[('prey', 'dummy')])
 #
-# class EFASTJSONNotebookTestCase(unittest.TestCase):
+
+#
+# class EFASTJSONNotebook2TestCase(unittest.TestCase):
 #
 #     def setUp(self):
 #         self.filename = 'efastlabtest.json'
 #
-#     # def tearDown(self):
-#     #     # # Get rid of json file
-#     #     if os.path.exists(self.filename):
-#     #         os.remove(self.filename)
+#     def tearDown(self):
+#         # # Get rid of json file
+#         if os.path.exists(self.filename):
+#             os.remove(self.filename)
 #
 #     def test_analyse(self):
 #         nb_pre = EFASTJSONNotebook(self.filename, True)
-#         self.lab = EFASTLab(nb_pre)
+#         self.lab = EFASTClusterLab(nb_pre, 'efast_test')
+#         print "{0} engines".format(self.lab.numberOfEngines())
 #
 #         model = LotkaVolterraModel()
 #         self.lab[LotkaVolterraModel.PARAM_ALPHA] = [1.5, 0.01, NORMAL_DISTRIBUTION]
@@ -94,6 +171,9 @@ class LotkaVolterraModel(epyc.Experiment):
 #
 #         print "Running Experiment"
 #         self.lab.runExperiment(RepeatedExperiment(model, 2))
+#         print "JOBS SENT"
+#
+#         print self.lab.pendingResults()
 #
 #         nb_post = EFASTJSONNotebook(self.filename, False)
 #         print "Analysing Experiment Results"
@@ -132,86 +212,6 @@ class LotkaVolterraModel(epyc.Experiment):
 #         print stats.ttest_ind(res_st[('prey', 'beta')], res_s1[('prey', 'dummy')])
 #         print stats.ttest_ind(res_st[('prey', 'sigma')], res_s1[('prey', 'dummy')])
 #         print stats.ttest_ind(res_st[('prey', 'delta')], res_s1[('prey', 'dummy')])
-#
-
-
-class EFASTJSONNotebook2TestCase(unittest.TestCase):
-
-    def setUp(self):
-        self.filename = 'efastlabtest.json'
-
-    # def tearDown(self):
-    #     # # Get rid of json file
-    #     if os.path.exists(self.filename):
-    #         os.remove(self.filename)
-
-    def test_analyse(self):
-        nb_pre = EFASTJSONNotebook(self.filename, True)
-        self.lab = EFASTClusterLab(nb_pre, 'efast_test')
-        print "{0} engines".format(self.lab.numberOfEngines())
-
-        model = LotkaVolterraModel()
-        self.lab[LotkaVolterraModel.PARAM_ALPHA] = [1.5, 0.01, NORMAL_DISTRIBUTION]
-        self.lab[LotkaVolterraModel.PARAM_BETA] = [1, 0.2, NORMAL_DISTRIBUTION]
-        self.lab[LotkaVolterraModel.PARAM_SIGMA] = [3, 0.2, NORMAL_DISTRIBUTION]
-        self.lab[LotkaVolterraModel.PARAM_DELTA] = [1, 0.01, NORMAL_DISTRIBUTION]
-
-        self.lab[LotkaVolterraModel.INIT_Q] = 10
-        self.lab[LotkaVolterraModel.INIT_P] = 5
-
-        model.set_time_params(20, 21)
-
-        resamples = 5  # NR
-        runs = 257  # NS
-        mi = 4.0
-
-        self.lab.set_sample_number(runs)
-        self.lab.set_resample_number(resamples)
-        self.lab.set_interference_factor(mi)
-
-        print "Running Experiment"
-        self.lab.runExperiment(RepeatedExperiment(model, 2))
-        print "JOBS SENT"
-
-        print self.lab.pendingResults()
-
-        # nb_post = EFASTJSONNotebook(self.filename, False)
-        # print "Analysing Experiment Results"
-        # res_s1, res_st = nb_post.generate_sensitivity_indices()
-        #
-        # s1_vals = [numpy.mean(res_s1[('prey',p)]) for p in ['alpha','beta','sigma','delta','dummy']]
-        # st_vals = [numpy.mean(res_st[('prey',p)]) for p in ['alpha','beta','sigma','delta','dummy']]
-        # N = len(s1_vals)
-        # s1_Std = [2*numpy.std(res_s1[('prey',p)]) for p in ['alpha','beta','sigma','delta','dummy']]
-        # st_Std = [2*numpy.std(res_s1[('prey',p)]) for p in ['alpha','beta','sigma','delta','dummy']]
-        # ind = np.arange(N)  # the x locations for the groups
-        # width = 0.8  # the width of the bars: can also be len(x) sequence
-        #
-        # p2 = plt.bar(ind, st_vals, width, bottom=[0, ] * N, yerr=st_Std)
-        # p1 = plt.bar(ind, s1_vals, width, yerr=s1_Std)
-        #
-        # plt.ylim((0,1))
-        #
-        # plt.ylabel('eFAST sensitivity')
-        # plt.title('Lotka-Volterra EFAST')
-        # plt.xticks(ind, (r'$\alpha$', r'$\beta$', r'$\sigma$', r'$\delta$', r'$dummy$'))
-        # plt.yticks(np.arange(0, 1, 0.1))
-        # plt.legend((p1[0], p2[0]), (r'$S_1$', r'$S_T$'))
-        #
-        # plt.savefig("LV.png")
-        #
-        # print res_st[('prey', 'alpha')], numpy.mean(res_st[('prey', 'alpha')]), numpy.std(res_st[('prey', 'alpha')])
-        # print res_st[('prey', 'delta')], numpy.mean(res_st[('prey', 'delta')]), numpy.std(res_st[('prey', 'delta')])
-        #
-        # print stats.ttest_ind(res_s1[('prey','alpha')], res_s1[('prey','dummy')])
-        # print stats.ttest_ind(res_s1[('prey', 'beta')], res_s1[('prey', 'dummy')])
-        # print stats.ttest_ind(res_s1[('prey', 'sigma')], res_s1[('prey', 'dummy')])
-        # print stats.ttest_ind(res_s1[('prey', 'delta')], res_s1[('prey', 'dummy')])
-        #
-        # print stats.ttest_ind(res_st[('prey', 'alpha')], res_s1[('prey', 'dummy')])
-        # print stats.ttest_ind(res_st[('prey', 'beta')], res_s1[('prey', 'dummy')])
-        # print stats.ttest_ind(res_st[('prey', 'sigma')], res_s1[('prey', 'dummy')])
-        # print stats.ttest_ind(res_st[('prey', 'delta')], res_s1[('prey', 'dummy')])
 
 if __name__ == '__main__':
     unittest.main()
